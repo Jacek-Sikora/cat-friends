bfe7966d330b85bbae47380cbfd46aa1cc7549ed
Jacek Sikora <jacek.sikora0@gmail.com>
2020-04-09 07:48:50 +0200

    Updates

---
+++ precache-manifest.5203134f7b7c6096f566bc5af82420fc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "925d1d15f667b68b9b9caca4654e2e6f",
+    "url": "/cat-friends/index.html"
+  },
+  {
+    "revision": "b628433163b8d524bb79",
+    "url": "/cat-friends/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "5bfa46502652f69293ac",
+    "url": "/cat-friends/static/css/main.f77f2eb6.chunk.css"
+  },
+  {
+    "revision": "b628433163b8d524bb79",
+    "url": "/cat-friends/static/js/2.79e767d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cat-friends/static/js/2.79e767d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bfa46502652f69293ac",
+    "url": "/cat-friends/static/js/main.7c8a4af1.chunk.js"
+  },
+  {
+    "revision": "40db964f8c5a5711f032",
+    "url": "/cat-friends/static/js/runtime-main.fdbeea21.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/cat-friends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
